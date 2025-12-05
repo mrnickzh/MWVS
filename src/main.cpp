83b79a7bc3db00589965ed7841c65ad86fabe6dd
mrnickzh <1234dxc@gmail.com>
2025-12-05 17:23:12 +0300

    more packets + mutexes

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,7 @@
 #include <ixwebsocket/IXNetSystem.h>
 #include <Server.hpp>
 #include "Protocol/ServerPacketHelper.hpp"
-
-std::map<ClientSession, ix::WebSocket*> clients;
+#include <mutex>
 
 int main() {
     ix::initNetSystem();
@@ -13,29 +12,39 @@ int main() {
     std::string host("127.0.0.1");
     ix::WebSocketServer server(port, host);
     Server& serverInstance = Server::getInstance();
+    std::mutex serverInstanceMutex;
 
-    serverInstance.setCallback([&](ClientSession session, std::vector<uint8_t> data) {
+    serverInstance.setCallback([&](ClientSession* session, std::vector<uint8_t> data) {
         std::string str(data.begin(), data.end());
-        clients[session]->sendBinary(str);
+        static_cast<ix::WebSocket*>(serverInstance.clients[session])->sendBinary(str);
     });
 
     server.setOnClientMessageCallback([&](std::shared_ptr<ix::ConnectionState> connectionState, ix::WebSocket& webSocket, const ix::WebSocketMessagePtr & msg) {
         std::string remoteaddr = std::string(connectionState->getRemoteIp()) + " " + std::to_string(connectionState->getRemotePort());
 
-        std::cout << "Client: " << remoteaddr << std::endl;
-
         if (msg->type == ix::WebSocketMessageType::Open)
         {
-            clients[ClientSession(remoteaddr)] = &webSocket;
+            std::cout << "Connected: " << remoteaddr << std::endl;
+            std::lock_guard<std::mutex> guard(serverInstanceMutex);
+            serverInstance.clients[new ClientSession(remoteaddr)] = (void*)&webSocket;
         }
         else if (msg->type == ix::WebSocketMessageType::Message && msg->binary)
         {
             std::vector<uint8_t> data(msg->str.begin(), msg->str.end());
-            ServerPacketHelper::decodePacket(ClientSession(remoteaddr), data);
+            auto it = std::find_if(serverInstance.clients.begin(), serverInstance.clients.end(), [remoteaddr](std::pair<ClientSession*, void*> f)->bool{ return f.first->clientaddress == remoteaddr; });
+            if (it != serverInstance.clients.end()) {
+                std::lock_guard<std::mutex> guard(serverInstanceMutex);
+                ServerPacketHelper::decodePacket(it->first, data);
+            }
         }
         else if (msg->type == ix::WebSocketMessageType::Close)
         {
-            clients.erase(ClientSession(remoteaddr));
+            std::cout << "Disconnected: " << remoteaddr << std::endl;
+            auto it = std::find_if(serverInstance.clients.begin(), serverInstance.clients.end(), [remoteaddr](std::pair<ClientSession*, void*> f)->bool{ return f.first->clientaddress == remoteaddr; });
+            if (it != serverInstance.clients.end()) {
+                std::lock_guard<std::mutex> guard(serverInstanceMutex);
+                serverInstance.clients.erase(it);
+            }
         }
     });
 
